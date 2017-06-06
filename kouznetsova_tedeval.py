326f0a97dca8e5b46901fafbe57216ba858d49bc
NoSurvivalValue <zoot.wowsquirrel@gmail.com>
2017-06-06 09:48:22 +0300

    Add files via upload

--- kouznetsova_tedeval.py
+++ kouznetsova_tedeval.py
@@ -44,7 +44,7 @@ def check_file(path):
             print('\nIncorrect format\n')
             exit()
         else:
-            print('\nFormat of the file: Confirmed')
+            print('\nFormat of the file: Confirmed\n')
 
 # Class of a tree node
 
@@ -217,12 +217,27 @@ def span_difference(parsed, gold):
         norm_diff.append(diff)
     return norm_diff
 
+# PATH LENGTH
+
+def path_length(parsed, gold):
+    lengths = []
+    for i in range(len(parsed)):
+        tree_diff = []
+        for l in range(len(parsed[i])):
+            diff = abs(len(parsed[i][l].path_to_root) - len(gold[i][l].path_to_root))
+            max_diff = len(parsed[i]) - 1
+            tree_diff.append(round(diff / max_diff, 4))
+        lengths.append(round(1 - round(sum(tree_diff)/len(tree_diff), 4), 4))
+    return lengths
+        
+            
+
 # NORMALIZATION - summing up all the metrics
 
 def normalize(metric1, metric2, metric3, metric4, metric5):
     normalization = []
     for i in range(len(metric1)):
-        norm = round(0.25 * metric1[i] + 0.25 * metric2[i] + 0.25 * metric3[i] + 0.2 * metric4[i] + 0.05 * metric5[i], 4)
+        norm = round(0.25 * metric1[i] + 0.1 * metric2[i] + 0.4 * metric3[i] + 0.2 * metric4[i] + 0.05 * metric5[i], 4)
         normalization.append(norm)
     return normalization
 
@@ -238,8 +253,8 @@ def average_score(option, gold1, gold2):
     metric22 = relations(option, gold2)
     metric13 = ped(option, gold1)
     metric23 = ped(option, gold2)
-    metric14 = span_difference(option, gold1)
-    metric24 = span_difference(option, gold2)
+    metric14 = path_length(option, gold1)
+    metric24 = path_length(option, gold2)
     metric15 = root_accuracy(option, gold1)
     metric25 = root_accuracy(option, gold2)
     norm1 = normalize(metric11, metric12, metric13, metric14, metric15)
@@ -362,7 +377,7 @@ def compare_to_gold():
     metric1 = accuracy(parsed, gold)
     metric2 = relations(parsed, gold)
     metric3 = ped(parsed, gold)
-    metric4 = span_difference(parsed, gold)
+    metric4 = path_length(parsed, gold)
     metric5 = root_accuracy(parsed, gold)
     normalization = normalize(metric1, metric2, metric3, metric4, metric5)
     print('\nWriting result file')
@@ -374,12 +389,12 @@ def compare_to_generalized_gold():
     gold_path = input('The path to the gold file for the parser: ')
     check_file(gold_path)
     gold_paths = [gold_path]
-    n = int(input('How many other gold standarts will contribute to the genralized gold standard? '))
+    n = int(input('\nHow many other gold standarts will contribute to the genralized gold standard? '))
     for i in range(n):
         gold_path = input('The path to the gold file: ')
         gold_paths.append(gold_path)
     golds = []
-    punct = input('Ignore punctuation in files? (yes/no) ')    
+    punct = input('\nIgnore punctuation in files? (yes/no) ')    
     if punct == 'yes':
         parsed = make_tree(parsed_path)
         for g in gold_paths:
@@ -400,7 +415,7 @@ def compare_to_generalized_gold():
     metric2 = relations(parsed, gold)
     metric3 = ped(parsed, gold)
     metric4 = span_difference(parsed, gold)
-    metric5 = root_accuracy(parsed, gold)
+    metric5 = path_length(parsed, gold)
     normalization = normalize(metric1, metric2, metric3, metric4, metric5)
     print('\nWriting result file with the gold standard')
     write_result('Result_with_gold.txt', gold, parsed, metric1, metric2, metric3, metric4, metric5, normalization)
@@ -408,8 +423,63 @@ def compare_to_generalized_gold():
     metric2b = relations(parsed, golds[-1])
     metric3b = ped(parsed, golds[-1])
     metric4b = span_difference(parsed, golds[-1])
-    metric5b = root_accuracy(parsed, golds[-1])
+    metric5b = path_length(parsed, golds[-1])
     normalization2 = normalize(metric1b, metric2b, metric3b, metric4b, metric5b)
     print('\nWriting result file with the generalized gold standard')
     write_result('Result_with_generalized_gold.txt', gold, parsed, metric1b, metric2b, metric3b, metric4b, metric5b, normalization2)
-    
+
+
+def compare_two_parsers():
+    parsed_path1 = input('The path to the first parsed file: ')
+    check_file(parsed_path1)
+    gold_path1 = input('The path to the gold file for the first parser: ')
+    check_file(gold_path1)
+    parsed_path2 = input('The path to the second parsed file: ')
+    check_file(parsed_path2)
+    gold_path2 = input('The path to the gold file for the second parser: ')
+    check_file(gold_path2)
+    punct = input('\nIgnore punctuation in files? (yes/no) ')    
+    if punct == 'yes':
+        parsed1 = make_tree(parsed_path1)
+        parsed2 = make_tree(parsed_path2)
+        gold1 = make_tree(gold_path1)
+        gold2 = make_tree(gold_path2)
+    else:
+        parsed1 = make_tree_wo_punctuation(parsed_path1)
+        parsed2 = make_tree_wo_punctuation(parsed_path2)
+        gold1 = make_tree_wo_punctuation(gold_path1)
+        gold2 = make_tree_wo_punctuation(gold_path2)
+    print('\nCreating a generalized gold standard')
+    generalized = generalize(gold1, gold2)
+    metric1 = accuracy(parsed1, gold1)
+    metric2 = relations(parsed1, gold1)
+    metric3 = ped(parsed1, gold1)
+    metric4 = span_difference(parsed1, gold1)
+    metric5 = path_length(parsed1, gold1)
+    normalization = normalize(metric1, metric2, metric3, metric4, metric5)
+    print('\nWriting result file for the first parser with the gold standard')
+    write_result('Result_with_gold_1.txt', gold1, parsed1, metric1, metric2, metric3, metric4, metric5, normalization)
+    metric1b = accuracy(parsed2, gold2)
+    metric2b = relations(parsed2, gold2)
+    metric3b = ped(parsed2, gold2)
+    metric4b = span_difference(parsed2, gold2)
+    metric5b = path_length(parsed2, gold2)
+    normalization2 = normalize(metric1b, metric2b, metric3b, metric4b, metric5b)
+    print('\nWriting result file for the second parser with the gold standard')
+    write_result('Result_with_gold_2.txt', gold2, parsed2, metric1b, metric2b, metric3b, metric4b, metric5b, normalization2)
+    metric1c = accuracy(parsed1, generalized)
+    metric2c = relations(parsed1, generalized)
+    metric3c = ped(parsed1, generalized)
+    metric4c = span_difference(parsed1, generalized)
+    metric5c = path_length(parsed1, generalized)
+    normalization3 = normalize(metric1c, metric2c, metric3c, metric4c, metric5c)
+    print('\nWriting result file for the first parser with the generalized gold standard')
+    write_result('Result_with_generalized_gold_1.txt', generalized, parsed1, metric1c, metric2c, metric3c, metric4c, metric5c, normalization3)
+    metric1d = accuracy(parsed2, generalized)
+    metric2d = relations(parsed2, generalized)
+    metric3d = ped(parsed2, generalized)
+    metric4d = span_difference(parsed2, generalized)
+    metric5d = path_length(parsed2, generalized)
+    normalization4 = normalize(metric1d, metric2d, metric3d, metric4d, metric5d)
+    print('\nWriting result file for the second parser with the generalized gold standard')
+    write_result('Result_with_generalized_gold_2.txt', generalized, parsed2, metric1d, metric2d, metric3d, metric4d, metric5d, normalization3)
